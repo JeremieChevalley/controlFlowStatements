c998bede31458056935fa6373ad311fd4797d962
Jérémie Noah Chevalley <pb83pyi@eduvaud.ch>
2024-09-13 14:42:00 +0200

    feat: break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,14 @@
 const numbers = [];
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
+    if (numbers.some(numbers => numbers >51)){ // Aidé de ChatGPT
+        break;
+    }
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+    }
+if (numbers.length<10) {
+    console.log(`Nombre d'opérations évitées : ${10 - numbers.length}`);
 }
